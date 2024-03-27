44c6d057e4226a53784e39f9fb3f92878ff6849f
Mohit Arora <me@aboutmohit.com>
2024-03-27 17:48:45 -0400

    Initial Commit

---
+++ TutorialApp/TutorialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialAppApp.swift
+//  TutorialApp
+//
+//  Created by Mohit Arora on 2024-03-27.
+//
+
+import SwiftUI
+
+@main
+struct TutorialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
